d19bbbdbe21ac84b6e49aa577bc8310edcdb7792
Matthew Steele <mfsairpwr@gmail.com>
2014-04-14 11:40:59 -0500

    Added missing files

---
+++ lib/filters/thumnailize.rb
@@ -0,0 +1,25 @@
+class Thumbnailize < Nanoc::Filter
+
+  identifier :thumbnailize
+  type       :binary
+
+  def run(filename, params={})
+
+    if(os() == :macosx)
+      system(
+        'sips',
+        '--resampleWidth',
+        params[:width].to_s,
+        '--out',
+        "png:#{output_filename}",
+        filename
+      )
+    else
+        cmd = "convert -flatten -thumbnail #{params[:width].to_s} #{filename}[0] png:#{output_filename}"
+        p cmd
+        out = `#{cmd}`
+        p out
+    end
+  end
+
+end
