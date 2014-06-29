cf1a3000ea18a96382ca87d8694d4e6933e49911
Matthew Steele <mfsairpwr@gmail.com>
2014-06-28 21:19:53 -0500

    Donation transaction javascript separated from donation_2 content.

---
+++ static/scripts/vancoServices.js
@@ -0,0 +1,171 @@
+var transaction = {}; //Create the global to store all data
+
+$('#amount-form :submit').click(function(event) {
+  event.preventDefault(); //End the form submission event now!
+  var amount = $(this).attr("value");
+  if(amount == "custom") {
+    amount = $('.enteredAmount').text();
+  }
+  transaction["amount"] = amount;
+  $('.amount').text(transaction['amount']);
+  $( "#amount-block" ).css("display", "none");
+  $( "#who-block" ).css("display", "block");
+});
+
+$('#who-form').submit(function(event) {
+  event.preventDefault(); //End the form submission event now!
+  var who = $("#who-form").serializeArray();
+  $.each(who, function() {
+    transaction[this.name] = this.value || '';
+  });
+    
+  $( "#who-block" ).css("display", "none");
+  $( "#CC-block" ).css("display", "block");
+});
+
+encrypto = function getNVP(a, b) {
+  $.ajax({
+    type: 'GET',
+    url: 'http://sa-cdc.org/scripts/vanco/nvpEncrypt.php',
+    crossDomain: true,
+    data: a,
+    dataType: 'jsonp',
+    success: function(data){ b(data); },
+    error: function (jqXHR, textStatus, errorThrown, data) {
+      //TODO do something useful
+      alert('Local: '+errorThrown);
+    }
+  });
+}
+
+wsNVP = function callWSNVP(a, b) {
+  $.ajax({
+    type: 'GET',
+    url: 'https://www.vancodev.com/cgi-bin/wsnvptest.vps',
+    crossDomain: true,
+    data: a,
+    dataType: 'jsonp',
+    success: function(data){ b(data); },
+    error: function (jqXHR, textStatus, errorThrown, data) {
+      alert('Vanco: '+errorThrown);
+    }
+  });
+}
+
+$('#CC-form').submit(function(event) {
+  event.preventDefault(); //End the form submission event now!
+
+  var acct = $( "#CC-form" ).serializeArray();
+  jQuery.each(acct, function() {
+    transaction[this.name] = this.value || '';
+  });
+
+  //Data to encrypt locally
+  var paymentData = {};
+  paymentData['requesttype'] = 'efttransparentredirect';
+  paymentData['urltoredirect'] = './';
+  paymentData['isdebitcardonly'] = 'isdebitcardonly' in transaction ?'Yes':'No';
+   
+  encrypto(paymentData, function(data) {
+
+    //Data to be handed over to VANCO only
+    data['name'] = transaction['last']+', '+transaction['first'];
+    data['billingaddr1'] = transaction['addr1'];
+    data['billingaddr2'] = transaction['addr2'];
+    data['billingcity'] = transaction['city'];
+    data['billingstate'] = transaction['state'];
+    data['billingzip'] = transaction['zip'];
+    data['accounttype'] = "CC";
+    data['name_on_card'] = transaction['first'] +' '+transaction['last'];
+    data['accountnumber'] = transaction['accountnumber'];
+    data['expyear'] = transaction['expyear'];
+    data['expmonth'] = transaction['expmonth'];
+
+    wsNVP(data, function(response) {
+
+      var cData = {};
+      cData['requesttype'] = 'eftaddcompletetransaction';
+      //Customer Parameters to encrypt locally
+      cData['customerref'] = response['customerref'];
+      cData['customerid'] = response['customerref'];
+      cData['customername'] = data['name'];
+      cData['customeraddress1'] = transaction['addr1'];
+      cData['customeraddress2'] = transaction['addr2'];
+      cData['customercity'] = transaction['city'];
+      cData['customerstate'] = transaction['state'];
+      cData['customerzip'] = transaction['zip'];
+      cData['customerphone'] = '210-555-5555';
+
+      //Payment Method Parameters
+      cData['paymentmethodref'] = response['paymentmethodref'];
+      cData['isdebitcardonly'] = paymentData['isdebitcardonly'];
+
+      //Amount Parameters w/ Funds
+      //cData['fundid_0'] = 'Endowment';
+      //cData['fundamount_0'] = '';
+
+      //Amount Parameters w/o Funds
+      cData['amount'] = transaction['amount'];
+
+      //Transaction Parameters
+      cData['startdate'] = '0000-00-00';
+      //cData['enddate'] = '0000-00-00';
+      cData['frequencycode'] = 'O';
+      cData['transactiontypecode'] = 'WEB';
+
+      //Flag for local server to submit to VANCO
+      cData['tovanco'] = true;
+
+      //Encrypt cData...
+      encrypto(cData, function(out) {
+        alert(JSON.stringify(out));
+      });
+    });
+  });
+
+  $( "#CC-block" ).css("display", "none");
+  $( "#confirm-block" ).css("display", "block");
+});
+
+
+//TODO: Evaluate what parts of these are still needed...
+$( "input[name=accounttype]" ).change(function() {
+  type = $( "input:radio[name=accounttype]:checked" ).val();
+  if(type == "CC") {
+    $( ".CC" ).css("display", "block");
+    $( ".CS" ).css("display", "none");
+  } else if(type == "C" || type == "S") {
+    $( ".CS" ).css("display", "block");
+    $( ".CC" ).css("display", "none");
+  }
+});
+
+$('#CS-form').submit(function() {
+  $.ajax({
+    type: "POST",
+      url: url,
+      data: { "sessionid":sessionid,
+              "nvpvar":nvpvar,
+              "accounttype":"CC",
+              "accountnumber": ccno,
+              "expmonth": mm,
+              "expyear": yy,
+              "email": email,
+              "name": custname,
+              "billingaddr1": custadd,
+              "billingcity": custcity, 
+              "billingstate": custstate,
+              "billingzip": custzip,
+              "name_on_card": custname },
+      dataType: 'jsonp',
+      async: true,
+      traditional: false,
+      success: function (data) {
+                 alert(JSON.stringify(data));
+               },
+      error: function (jqXHR, textStatus, errorThrown, data) {
+               alert(errorThrown);
+             }
+  });
+});
+
