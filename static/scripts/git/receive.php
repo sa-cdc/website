d74b14fe58f569f94315c665f30fae5503874318
Matthew Steele <mfsairpwr@gmail.com>
2015-09-09 13:51:32 -0400

    Added cases to receive.php

--- static/scripts/git/receive.php
+++ static/scripts/git/receive.php
@@ -1,4 +1,16 @@
 <?php
   $input= json_decode( $_POST, TRUE ); //convert JSON into array
   print_r(json_encode($input));
+  
+  switch($input['ref']){
+    case 'refs/heads/master':
+      print 'change received on master';
+      break;
+    case 'refs/heads/dev':
+      print 'change received on dev';
+      break;
+    default:
+      print 'change received not affecting master or dev branches';
+      break;
+  }
 ?>
