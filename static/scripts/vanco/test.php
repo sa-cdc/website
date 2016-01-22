71d8bab0d18f8de6e4c56fc5be60fc09be95ae11
Matthew Steele <mfsairpwr@gmail.com>
2016-01-22 14:36:37 -0500

    Update test.php

--- static/scripts/vanco/test.php
+++ static/scripts/vanco/test.php
@@ -2,7 +2,7 @@
 require('vancoFunctions.inc');
 
 #clear_open_session();
-$result = array('output' => isValidSession());
+$result = array('output' => isValidSession(null));
 header('Content-Type: text/html');
 echo '<html>'.json_encode($result).'</html>';
 ?>
