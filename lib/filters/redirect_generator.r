2a7219856f208fbc7ea1a33c43265695b2630f0a
Matthew Steele <mfsairpwr@gmail.com>
2016-01-22 08:49:58 -0500

    Create redirect_generator.r

---
+++ lib/filters/redirect_generator.r
@@ -0,0 +1,18 @@
+class RedirectGenerator
+  def self.generate(redirects, items)
+    redirect_template = ERB.new(File.read('redirect.html.erb'))
+
+    redirects.each do |pairs|
+      pairs.each_pair do |url, aliases|
+
+        if aliased_item = items.find{|item| item.identifier == "/old_posts#{url}/" }
+          aliases.each do |alias_url|
+            redirect = {:url => url, :title => aliased_item[:title]}
+            items << Nanoc3::Item.new(redirect_template.result(binding), { :redirect => true }, alias_url)
+          end
+        end
+
+      end
+    end
+  end
+end
