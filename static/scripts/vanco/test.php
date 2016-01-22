a7a66cef7ec684e251b35d7c7c691a8a3eca01dc
Matthew Steele <mfsairpwr@gmail.com>
2016-01-22 15:10:07 -0500

    Update test.php

--- static/scripts/vanco/test.php
+++ static/scripts/vanco/test.php
@@ -4,5 +4,5 @@ require('vancoFunctions.inc');
 #clear_open_session();
 $result =  open_session_test();
 header('Content-Type: text/html');
-echo '<html>'.print_r($result).'</html>';
+print_r($result);
 ?>
