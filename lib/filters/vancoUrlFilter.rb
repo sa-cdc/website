f96a28e68bb8881a22a017c3c8b39751566f405a
Matthew Steele <mfsairpwr@gmail.com>
2016-01-29 11:13:37 -0500

    Create vancoUrlFilter.rb

---
+++ lib/filters/vancoUrlFilter.rb
@@ -0,0 +1,8 @@
+class VancoURLFilter < Nanoc::Filter
+  identifier :vanco_url
+
+  def run(content, params = {})
+    url = `php -f static/scripts/getNCP_URL`
+    content.gsub('VANCO_URL', url)
+  end
+end
