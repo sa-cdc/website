ffb237a358e23e59a0b52f2ddbec2978c99cf11b
Matthew Steele <mfsairpwr@gmail.com>
2016-02-03 08:54:20 -0500

    Update vancoUrlFilter.rb

--- lib/filters/vancoUrlFilter.rb
+++ lib/filters/vancoUrlFilter.rb
@@ -2,8 +2,8 @@ class VancoURLFilter < Nanoc::Filter
   identifier :vanco_url
 
   def run(content, params = {})
-    url = `php -f ./static/scripts/vanco/getNVP_URL.php`
-    #url = `echo $PWD`
+    #url = `php -f ./static/scripts/vanco/getNVP_URL.php`
+    url = `echo $PWD`
     content.gsub('VANCO_WSNVP', url)
   end
 end
