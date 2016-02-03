5b0fcbb7749c0f69b368d3bb01010919da034a36
Matthew Steele <mfsairpwr@gmail.com>
2016-02-03 14:19:05 -0500

    Update vancoUrlFilter.rb

--- lib/filters/vancoUrlFilter.rb
+++ lib/filters/vancoUrlFilter.rb
@@ -6,7 +6,7 @@ class VancoURLFilter < Nanoc::Filter
     branch = `basename $PWD`
     data = `cat ~/conf.inc.#{branch}`
     #GREP data for what we need...
-    m = /.*/.match('VANCO_WSNVP')
+    m = /.*/.match("VANCO_WSNVP")
     url = "\'" + m[0] + "\'"
     content.gsub('VANCO_WSNVP', url)
   end
