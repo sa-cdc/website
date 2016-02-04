05a980dd3e1e5379e1494c4659002269fa95a5da
Matthew Steele <mfsairpwr@gmail.com>
2016-02-03 21:46:19 -0500

    Update vancoUrlFilter.rb

--- lib/filters/vancoUrlFilter.rb
+++ lib/filters/vancoUrlFilter.rb
@@ -7,11 +7,11 @@ class VancoURLFilter < Nanoc::Filter
     data = `cat ~/conf.inc.#{branch}`
     #GREP data for what we need...
     nvp_url = /^.*VANCO_WSNVP\',\s*\'(.*)\'.*$/.match(data)
-    xml_url = /^.*VANCO_XML\',\s*\'(.*)\'.*$/.match(data)
+    xml_url = /^.*VANCO_WS\',\s*\'(.*)\'.*$/.match(data)
     dev_mode = /^.*VANCO_DEV_MODE\',\s*\'(.*)\'.*$/.match(data)
     
     content.gsub('VANCO_WSNVP', nvp_url[1])
-    content.gsub('VANCO_WS', xml_url[1])
+    content.gsub('VANCO_XML', xml_url[1])
     content.gsub('VANCO_DEV_MODE', dev_mode[1])
   end
 end
