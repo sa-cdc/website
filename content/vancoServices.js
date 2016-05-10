aab484bbd94704bed718f63448598889fddd7802
Matthew Steele <mfsairpwr@gmail.com>
2016-05-10 10:20:56 -0400

    Update vancoServices.js

--- content/vancoServices.js
+++ content/vancoServices.js
@@ -184,7 +184,7 @@ $().ready(function() {
 var checkingVancoService = encrypto(fakeData, function(data) {
   $.ajax({ type: 'GET', url: 'VANCO_WSNVP', timeout: 4000, crossDomain: true, data: data, dataType: 'jsonp'});
 };
-
+alert(checkingVancoService.status());
 checkingVancoService.then(function(){
         $('#donationApp').removeClass("hidden");
       },
