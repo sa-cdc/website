c1ccdbbf703b0cd4c8bc6d45e7b17e1b80547767
Matthew Steele <mfsairpwr@gmail.com>
2016-05-11 13:35:28 -0400

    Update email.php

--- static/scripts/vanco/email.php
+++ static/scripts/vanco/email.php
@@ -1,12 +1,24 @@
 <?php
-// The message
-$files = scandir('../../../../');
-$avail = var_export($files, true);
-$message = "$avail";
+$ref = $_GET['ref'];
+$name = $_GET['name'];
+$amount = $_GET['amount'];
+$email = $_GET['email'];
+$phone = $_GET['phone'];
+//TODO: Validate this data - never trust user input
 
+//----
+$files = scandir('../../../../');
+
+if(!in_array($ref,$files)) {
+  //No matching reference found
+  //TODO: Send exception json
+  return;
+}
+
+$message = "$name is contributing $amount. Phone: $phone. Email: $email";
 // In case any of our lines are larger than 70 characters, we should use wordwrap()
 $message = wordwrap($message, 70, "\r\n");
 
 // Send
-mail('mfsairpwr@gmail.com', 'My Subject', $message);
+mail('mfsairpwr@gmail.com', 'Donation', $message);
 ?>
