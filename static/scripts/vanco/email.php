9770207e3a77eca52b883de547a6e82f3798827e
Matthew Steele <mfsairpwr@gmail.com>
2016-05-11 13:07:59 -0400

    Update email.php

--- static/scripts/vanco/email.php
+++ static/scripts/vanco/email.php
@@ -1,6 +1,8 @@
 <?php
 // The message
-$message = "Line 1\r\nLine 2\r\nLine 3";
+$files = scandir('./');
+$avail = var_export($files, true);
+$message = "$avail";
 
 // In case any of our lines are larger than 70 characters, we should use wordwrap()
 $message = wordwrap($message, 70, "\r\n");
