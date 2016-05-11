043763000dda199508b572c2d46163679747d177
Matthew Steele <mfsairpwr@gmail.com>
2016-05-11 13:00:16 -0400

    Added logic to hold a valid reference

--- static/scripts/vanco/storeRef.php
+++ static/scripts/vanco/storeRef.php
@@ -1,5 +1,22 @@
 <?php
+if(!ctype_digit($_GET['ref'])) {
+  header('Content-Type: application/json');
+  $array = ["status" => "invalid data"];
+  echo $_GET['callback'] . '('.json_encode($array).')';
+}
 
-$handle = fopen($ref, "w");
+$ref = (int)$_GET['ref'];
+
+if($ref <= 0) {
+  header('Content-Type: application/json'); 
+  $array = ["status" => "invalid data"];
+  echo $_GET['callback'] . '('.json_encode($array).')';
+}
+
+$handle = fopen("$ref", "w");
 fclose($handle);
+$array = ["status" => "invalid data"];
+header('Content-Type: application/json');
+echo $_GET['callback'] . '('.json_encode($array).')';
+
 ?> 
