7ad613de73a0cf28cd7ac6227ea55a5c7523dc56
Matthew Steele <mfsairpwr@gmail.com>
2016-05-16 12:00:40 -0400

    Update email.php

--- static/scripts/vanco/email.php
+++ static/scripts/vanco/email.php
@@ -16,11 +16,10 @@ if(!in_array($ref,$files) && $ref != 42) {
 }
 
 // In case any of our lines are larger than 70 characters, we should use wordwrap()
-$message = wordwrap("$message", 70, "\r\n");
-$headers = 'From: sacdc@sachristiandental.org' . "\r\n" .
-    'Reply-To: susan@sachristiandental.org' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
-    $recipient = 'mfsairpwr@gmail.com';
+$message = trim($message);
+$message = wordwrap($message, 70, "\r\n");
+$headers = "From: sacdc@sachristiandental.org\r\n";
+$recipient = "mfsairpwr@gmail.com";
 
 // Send
 $mailed = mail($recipient, 'Donation', $message, $headers);
