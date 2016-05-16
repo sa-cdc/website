42555eac7dfcf19b06ca865d384231685c9d0cf7
Matthew Steele <mfsairpwr@gmail.com>
2016-05-16 10:48:19 -0400

    Update email.php

--- static/scripts/vanco/email.php
+++ static/scripts/vanco/email.php
@@ -17,9 +17,13 @@ if(!in_array($ref,$files) && $ref != 42) {
 
 // In case any of our lines are larger than 70 characters, we should use wordwrap()
 $message = wordwrap($message, 70, "\r\n");
+$headers = 'From: sacdc@sachristiandental.org' . "\r\n" .
+    'Reply-To: susan@sachristiandental.org' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+    $recipient = 'mfsairpwr@gmail.com';
 
 // Send
-$mailed = mail('mfsairpwr@gmail.com', 'Donation', $message);
+$mailed = mail($recipient, 'Donation', $message, $headers);
 header('Content-Type: application/json'); 
 $array = $mailed?["status" => "$message"]:["status" => "failed to email"];
 echo $_GET['callback'] . '('.json_encode($array).')'; 
