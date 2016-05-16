72437b85c8992e5a28a924b8f5286b5bb8c7823b
Matthew Steele <mfsairpwr@gmail.com>
2016-05-16 13:50:29 -0400

    Update email.php

--- static/scripts/vanco/email.php
+++ static/scripts/vanco/email.php
@@ -24,7 +24,7 @@ if(!in_array($ref,$files) && $ref != 42) {
 // In case any of our lines are larger than 70 characters, we should use wordwrap()
 $message = trim($message);
 $message = wordwrap($message, 70, "\r\n");
-$headers = "From: sacdc@stage.sachristiandental.org\r\n";
+$headers = "From:sacdc@stage.sachristiandental.org \r\n";
 $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 $recipient = "mfsairpwr@gmail.com";
 
