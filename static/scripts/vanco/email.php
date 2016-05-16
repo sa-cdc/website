7bc852f3b080c043db9400b0b06f296750ce6c56
Matthew Steele <mfsairpwr@gmail.com>
2016-05-16 12:53:44 -0400

    Update email.php

--- static/scripts/vanco/email.php
+++ static/scripts/vanco/email.php
@@ -17,14 +17,14 @@ if(!in_array($ref,$files) && $ref != 42) {
   //TODO: Send exception json
   header('Content-Type: application/json'); 
   $array = ["status" => "invalid data"]; 
-  echo $_GET['callback'] . '('.json_encode($array).')';
+  echo '('.json_encode($array).')';
   return;
 }
 
 // In case any of our lines are larger than 70 characters, we should use wordwrap()
 $message = trim($message);
 $message = wordwrap($message, 70, "\r\n");
-$headers = "From: sacdc@sachristiandental.org\r\n";
+$headers = "From: sacdc@stage.sachristiandental.org\r\n";
 $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 $recipient = "mfsairpwr@gmail.com";
 
@@ -32,7 +32,7 @@ $recipient = "mfsairpwr@gmail.com";
 $mailed = mail($recipient, 'Donation', $message, $headers);
 header('Content-Type: application/json'); 
 $array = $mailed?["status" => "$message"]:["status" => "failed to email"];
-echo $_GET['callback'] . '('.json_encode($array).')';
+echo '('.json_encode($array).')';
 if($ref != 42) {
   unlink("../../../../$ref");
 }
