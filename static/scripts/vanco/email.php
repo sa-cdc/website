ded0e04ce46a592870110260871decaee657caaa
Matthew Steele <mfsairpwr@gmail.com>
2016-05-16 15:06:26 -0400

    Update email.php

--- static/scripts/vanco/email.php
+++ static/scripts/vanco/email.php
@@ -10,19 +10,6 @@ require_once('Mail.php');
    
 $recipients = 'matthew.steele.8@us.af.mil';
 
-$headers['From']    = 'sacdcorg@sachristiandental.org';
-$headers['To']      = 'matthew.steele.8@us.af.mil';
-$headers['Subject'] = 'Test message';
-
-$body = 'Test message';
-
-$params['sendmail_path'] = '/usr/lib/sendmail';
-
-// Create the mail object using the Mail::factory method
-$mail_object =& Mail::factory('sendmail', $params);
-
-$mail_object->send($recipients, $headers, $body);
-
 $message = $_GET['message'];
 $ref = $_GET['ref'];
 //TODO: Validate this data - never trust user input
@@ -40,15 +27,16 @@ if(!in_array($ref,$files) && $ref != 42) {
 }
 
 // In case any of our lines are larger than 70 characters, we should use wordwrap()
-$message = trim($message);
-$message = wordwrap($message, 70, "\n");
-$headers = "From:sacdc@stage.sachristiandental.org\n";
-$headers .= "Content-Type: text/html; charset=ISO-8859-1\n";
-$recipient = "mfsairpwr@gmail.com";
+$headers['From']    = 'sacdcorg@sachristiandental.org';
+$headers['To']      = $recipients;
+$headers['Subject'] = 'Donation '.$ref;
+$body = $message;
+$params['sendmail_path'] = '/usr/lib/sendmail';
+// Create the mail object using the Mail::factory method
+$mail_object =& Mail::factory('sendmail', $params);
+//Send
+$mailed = $mail_object->send($recipients, $headers, $body);
 
-// Send
-//$mailed = mail($recipient, 'Donation', $message, $headers);
-$mailed = false;
 header('Content-Type: application/json'); 
 $array = $mailed?["status" => "$message"]:["status" => "failed to email"];
 echo '('.json_encode($array).')';
