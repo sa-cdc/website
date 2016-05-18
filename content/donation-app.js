4a4373861dfc1cd02c3af0bb3b1bb19f675b0ac5
Matthew Steele <mfsairpwr@gmail.com>
2016-05-18 15:06:16 -0400

    Update donation-app.js

--- content/donation-app.js
+++ content/donation-app.js
@@ -1,17 +1,8 @@
 angular.module('app', ['ui.bootstrap'])
 .controller('mainController', function($scope) {
   
-  // BUTTONS ======================
-  
-  // define some random object and button values
-  $scope.bigData = {};
-  
-  $scope.bigData.breakfast = false;
-  $scope.bigData.lunch = false;
-  $scope.bigData.dinner = false;
-  
-  // COLLAPSE =====================
-  $scope.isCollapsed = false;
+  $scope.payment = {};
+  $scope.client = {};
   
 });
 
