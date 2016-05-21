6a28c020d7d25f9f7b68c20f232bfdcfe5f6c35f
Matthew Steele <mfsairpwr@gmail.com>
2016-05-21 06:42:17 -0400

    Update donation-app.js

--- content/donation-app.js
+++ content/donation-app.js
@@ -17,11 +17,13 @@ angular.module('donation-app', [])
   $scope.breadcrumb = 1;
   $scope.amountFormSubmit = function(isValid) {
     if(isValid) {
+      breadrumb = 2;
       toggleWho();
     }
   };
   $scope.whoFormSubmit = function(isValid) {
     if(isValid) {
+      breadcrumb = 3;
       togglePayment($scope.client, $scope.vanco);
     }
   };
