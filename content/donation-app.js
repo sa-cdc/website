71bd50fd1bbf4b45025620f510a207f28dee1957
Matthew Steele <mfsairpwr@gmail.com>
2016-05-26 14:09:51 -0400

    Update donation-app.js

--- content/donation-app.js
+++ content/donation-app.js
@@ -10,9 +10,9 @@ donationApp.controller('mainController', function($scope, vancoAPI) {
   $scope.vancoReachable = false;
   
   $scope.checkingVanco = vancoAPI.testWSNVP();
-  $scope.checkingVanco.finally(function(){
-    $scope.loading = false;
-  });
+  //$scope.checkingVanco.finally(function(){
+  //  $scope.loading = false;
+  //});
   $scope.checkingVanco.then(
     function(){
       $scope.vancoReachable = true;
