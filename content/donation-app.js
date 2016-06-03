4d35c8be6dff2a74b030bc7b6811a02d44e156d5
Matthew Steele <mfsairpwr@gmail.com>
2016-06-02 20:37:27 -0400

    debugging

--- content/donation-app.js
+++ content/donation-app.js
@@ -136,6 +136,7 @@ donationApp.factory('vancoAPI', function($http, $httpParamSerializer){
   }
 
   service.signNVP1 = function(data){
+    console.log("From NVP1: "+data);
     return $http.post('/static/scripts/vanco/nvpEncrypt.php', data);
   }
   service.signNVP = function(data){
