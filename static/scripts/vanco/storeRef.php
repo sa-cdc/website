0aa5476d7952abc1425b0fd53ae3402c8e46ce3c
Matthew Steele <mfsairpwr@gmail.com>
2016-09-12 20:35:17 -0400

    Update storeRef.php

--- static/scripts/vanco/storeRef.php
+++ static/scripts/vanco/storeRef.php
@@ -3,37 +3,32 @@
 $post = file_get_contents('php://input');
 $post = json_decode($post);
 
+header('Content-Type: application/json');
+
 if($post['ref'] == null) {
-  $array = json_encode(["status" => "no data"]);
-  header('Content-Type: application/json');
-  echo $array;
+  echo json_encode(["status" => "no data"]);
   return;
 } else {
   $ref = $post['ref'];
-  echo $ref;
+  echo json_encode($post);
+  return;
 }
 
 if(!ctype_digit($ref)) {
-  header('Content-Type: application/json');
-  $array = json_encode(["status" => "invalid data", "ref" => $ref]);
-  echo $array;
+  echo json_encode(["status" => "invalid data", "ref" => $ref]);
   return;
 }
 
 $ref = (int)$ref;
 
 if($ref <= 0) {
-  $array = json_encode(["status" => "reference < 0", "ref" => $ref]);
-  header('Content-Type: application/json'); 
-  echo $array;
+  echo json_encode(["status" => "reference < 0", "ref" => $ref]);
   return;
 }
 
 $handle = fopen("../../../../$ref", "w");
 fclose($handle);
-$array = json_encode(["status" => "success"]);
-header('Content-Type: application/json');
-echo $array;
+echo json_encode(["status" => "success"]);
 return;
 
-?> 
+?>
